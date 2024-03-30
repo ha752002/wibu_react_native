c9e9c00d12c85f3eb3598e5b9f9e05bb3c12ca1f
nguyenvanhoang0 <hoang03759@gmail.com>
2024-03-30 18:43:40 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { STORYBOOK_ENABLED } from '@env';
+import WibuCheckBox from './wibu-ui/WibuCheckBox/WibuCheckBox.tsx';
 
 function App() {
   useEffect(() => {
@@ -11,9 +12,14 @@ function App() {
     const StoryBook = require('../.storybook').default;
     return <StoryBook />;
   }
+
   return (
     <View>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <WibuCheckBox checked={'selected'} label={'aaaaaaaa'} />
+      <WibuCheckBox checked={'selected'} label={'dsdsdsd'} />
+      <WibuCheckBox checked={'selected'} label={'asa'} />
+      <WibuCheckBox checked={'selected'} label={'sas'} />
     </View>
   );
 }
--- src/wibu-ui/WibuCheckBox/WibuCheckBox.tsx
+++ src/wibu-ui/WibuCheckBox/WibuCheckBox.tsx
@@ -1,24 +1,30 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { WibuCheckBoxProps } from './WibuCheckBox.types.ts';
 import { useThemeStyles } from '../../hooks/useThemeStyles.ts';
 import { styleCreator } from './WibuCheckBox.styles.ts';
 
 const WibuCheckBox = (props: WibuCheckBoxProps) => {
-  const { label, checked, disabled } = props;
+  const { label, disabled } = props;
   const styles = useThemeStyles(styleCreator, props, []);
-
+  const [isChecked, setIsChecked] = useState<boolean>(false);
   const renderCheckBox = () => {
-    if (checked === 'selected') {
-      return <Text style={styles.checkBoxStyle}>✓</Text>;
-    } else if (checked === 'unselected') {
-      return <Text style={styles.checkBoxStyle}>-</Text>;
+    if (isChecked) {
+      return (
+        <Text style={[styles.checkBoxStyle, { backgroundColor: 'red' }]}>
+          ✓
+        </Text>
+      );
     }
     return <Text style={styles.checkBoxStyle} />;
   };
 
   return (
-    <TouchableOpacity disabled={disabled}>
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={() => setIsChecked(!isChecked)}
+    >
       <View style={[styles.checkBoxContainer]}>
         <View>{renderCheckBox()}</View>
         <Text style={[styles.label]}>{label}</Text>
