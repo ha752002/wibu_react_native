ba549e9d29c0831923a22193e75460cc61d0a905
Hong Ha <honggha75@gmail.com>
2024-04-08 09:00:48 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { STORYBOOK_ENABLED } from '@env';
 import WibuView from '~/wibu-ui/WibuView/WibuView.tsx';
+import { FlatList } from 'react-native';
+import WibuText from '~/wibu-ui/WibuText/WibuText.tsx';
 
 function App() {
   useEffect(() => {
@@ -12,9 +13,149 @@ function App() {
     const StoryBook = require('../.storybook').default;
     return <StoryBook />;
   }
+
+  const list = [
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+    {
+      name: 'ha',
+      age: 1,
+    },
+  ];
+
   return (
-    <WibuView>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <WibuView display={'flex'} gap={4} flexDirection={'column'}>
+      <FlatList
+        data={list}
+        ItemSeparatorComponent={() => {
+          return <WibuView height={5} />;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <WibuView padding={10} backgroundColor={'green'}>
+              <WibuText color={'fgWhite'}>{item.name}</WibuText>
+            </WibuView>
+          );
+        }}
+      />
     </WibuView>
   );
 }
