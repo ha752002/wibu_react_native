bd930cc4361bb645f6b065ba20642a33a826b323
Hong Ha <honggha75@gmail.com>
2024-04-09 09:09:59 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import { STORYBOOK_ENABLED } from '@env';
-import WibuView from '~/wibu-ui/WibuView/WibuView.tsx';
-import { FlatList } from 'react-native';
-import WibuText from '~/wibu-ui/WibuText/WibuText.tsx';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Test } from '~/Test.tsx';
 
 function App() {
   useEffect(() => {
@@ -14,149 +13,10 @@ function App() {
     return <StoryBook />;
   }
 
-  const list = [
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-    {
-      name: 'ha',
-      age: 1,
-    },
-  ];
-
   return (
-    <WibuView display={'flex'} gap={4} flexDirection={'column'}>
-      <FlatList
-        data={list}
-        // ItemSeparatorComponent={() => {
-        //   return <WibuView height={5} />;
-        // }}
-        renderItem={({ item }) => {
-          return (
-            <WibuView padding={10} backgroundColor={'green'}>
-              <WibuText color={'fgWhite'}>{item.name}</WibuText>
-            </WibuView>
-          );
-        }}
-      />
-    </WibuView>
+    <GestureHandlerRootView>
+      <Test />
+    </GestureHandlerRootView>
   );
 }
 
